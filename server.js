b6bca806c69a54d94437b16b1399b65b0cea3a20
letsdothiz <136943676+letsdothiz@users.noreply.github.com>
2023-08-29 11:15:31 -0400

    server.js port condition update

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const express = require('express')
 const app = express()
+const connectDB = require('./config/database')
+
 const homeRoutes = require('./routes/home')
 const bodyParser = require('body-parser')
-const connectDB = require('./config/database')
 const defaultPORT = 3000
 require('dotenv').config({path: './config/.env'})
 connectDB();
 
-app.set('view engine', 'ejs') //which view engine to use , eventually react
-app.use(express.static('public')) //hosts all of our static files// to pull data out of the request body ie.e. body parser
+app.set('view engine', 'ejs') 
+app.use(express.static('public')) 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use('/', homeRoutes)
 
-app.listen(defaultPORT || process.env.PORT, ()=>{
+app.listen(process.env.PORT || defaultPORT , ()=>{
     console.log('Server is running...')
 })
